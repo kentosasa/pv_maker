1f8adb8cc735d4ee85a769ef575a6f2dfa483da8
kento sasamoto <knote0721@gmail.com>
2017-03-02 17:53:04 +0900

    画像配列を返す(FireFoxのみ

--- es6/synthesis.es6
+++ es6/synthesis.es6
@@ -1,6 +1,6 @@
 var data = {
   imageUrl: 'http://1.bp.blogspot.com/_HmZhi2U5N_w/TDo7b5ttEYI/AAAAAAAABF8/VwRvxQd8_1o/s1600/IMG_0002.PNG',
-  text: 'これは動画を簡単に作成できるツールです。',
+  text: 'これは動画を簡単に\n作成できるツールです。',
   movie: './img/movie.mov',
   backColor: '#333333',
   textColor: '#ffffff'
@@ -9,12 +9,15 @@ var data = {
 // 定数
 var width = 1280
 var height = 720
-var frame = './img/frame.png'
+var iphoneImage = './img/frame.png'
+var frameRate = 10
 var time = 0
+
 var canvas
 var video
 var ctx
 var animationTimer
+var exportImages = []
 
 // 初期化処理
 var initialize = function () {
@@ -22,25 +25,26 @@ var initialize = function () {
   video.addEventListener('loadeddata', function() {
     video.play()
   })
+
   canvas = document.getElementById('canvas')
   canvas.width = width
   canvas.height = height
   ctx = canvas.getContext('2d')
   animationTimer = setInterval(draw, 100);
-
-  ctx.fillStyle = data.backColor
-  ctx.fillRect(0, 0, width, height)
 }
 
 var draw = function () {
   time = time + 1
+  ctx.fillStyle = data.backColor
+  ctx.fillRect(0, 0, width, height)
 
   // テキストの描画
-  drawText(data.text + time)
+  drawText(data.text +'\n' + time, 50, 300)
 
   Promise.resolve()
     .then(function() {
-      return drawImage(frame, width/2, 0, 450, height)
+      //枠の描画
+      return drawImage(iphoneImage, width/2, 0, 450, height)
     })
     .then(function () {
       return new Promise(function(resolve, reject) {
@@ -48,23 +52,26 @@ var draw = function () {
         resolve('taskB death')
       })
     })
+    .then(function () {
+      return new Promise(function(resolve, reject) {
+        if (time < 100) {
+          exportImages.push(exportPng())
+        } else {
+          console.log(exportImages)
+          clearInterval(animationTimer)
+        }
+      })
+    })
     .catch(function (error) {
       console.log(error)
     })
-
-  // iPhoneの枠を描画
-  // drawImage(frame, width/2, 0, 450, height)
-
-  // drawImage(data.imageUrl, 758, 168, 216, 393)
-  // drawVideo()
-  //exportPng()
 }
 
 // 非同期処理
 var drawImage = function (url, x, y, width, height) {
     return new Promise(function(resolve, reject) {
     var img = new Image()
-    img.src = frame
+    img.src = url
     img.onload = function() {
       ctx.drawImage(img, x, y, width, height)
       resolve('draw image');
@@ -72,21 +79,30 @@ var drawImage = function (url, x, y, width, height) {
   })
 }
 
-var drawText = function (text) {
+var drawText = function (text, x, y) {
   ctx.fillStyle = data.textColor
-  ctx.font = "24px 'メイリオ'";
-  ctx.fillText(text, 10, 50)
+  ctx.font = "36px 'メイリオ'";
+  var textList = text.split('\n');
+  var lineHeight = ctx.measureText("あ").width;
+  textList.forEach(function(text, i) {
+    ctx.fillText(text, x, y+lineHeight*i);
+  })
 }
 
 var drawVideo = function () {
   ctx.drawImage(video, 758, 168, 216, 393)
 }
 
-
 // 現在のcnavasのモノをbase64で返す
 var exportPng = function () {
   var data = canvas.toDataURL("image/png");
-  console.log(data)
+  return data
+}
+
+var exportVideo = function () {
+  var output = encoder.compile();
+  var url = webkitURL.createObjectURL(output);
+  console.log('url')
 }
 
 window.onload = function() {
--- js/synthesis.js
+++ js/synthesis.js
@@ -2,7 +2,7 @@
 
 var data = {
   imageUrl: 'http://1.bp.blogspot.com/_HmZhi2U5N_w/TDo7b5ttEYI/AAAAAAAABF8/VwRvxQd8_1o/s1600/IMG_0002.PNG',
-  text: 'これは動画を簡単に作成できるツールです。',
+  text: 'これは動画を簡単に\n作成できるツールです。',
   movie: './img/movie.mov',
   backColor: '#333333',
   textColor: '#ffffff'
@@ -11,12 +11,15 @@ var data = {
 // 定数
 var width = 1280;
 var height = 720;
-var frame = './img/frame.png';
+var iphoneImage = './img/frame.png';
+var frameRate = 10;
 var time = 0;
+
 var canvas;
 var video;
 var ctx;
 var animationTimer;
+var exportImages = [];
 
 // 初期化処理
 var initialize = function initialize() {
@@ -24,46 +27,49 @@ var initialize = function initialize() {
   video.addEventListener('loadeddata', function () {
     video.play();
   });
+
   canvas = document.getElementById('canvas');
   canvas.width = width;
   canvas.height = height;
   ctx = canvas.getContext('2d');
   animationTimer = setInterval(draw, 100);
-
-  ctx.fillStyle = data.backColor;
-  ctx.fillRect(0, 0, width, height);
 };
 
 var draw = function draw() {
   time = time + 1;
+  ctx.fillStyle = data.backColor;
+  ctx.fillRect(0, 0, width, height);
 
   // テキストの描画
-  drawText(data.text + time);
+  drawText(data.text + '\n' + time, 50, 300);
 
   Promise.resolve().then(function () {
-    return drawImage(frame, width / 2, 0, 450, height);
+    //枠の描画
+    return drawImage(iphoneImage, width / 2, 0, 450, height);
   }).then(function () {
     return new Promise(function (resolve, reject) {
       drawVideo();
       resolve('taskB death');
     });
+  }).then(function () {
+    return new Promise(function (resolve, reject) {
+      if (time < 100) {
+        exportImages.push(exportPng());
+      } else {
+        console.log(exportImages);
+        clearInterval(animationTimer);
+      }
+    });
   }).catch(function (error) {
     console.log(error);
   });
-
-  // iPhoneの枠を描画
-  // drawImage(frame, width/2, 0, 450, height)
-
-  // drawImage(data.imageUrl, 758, 168, 216, 393)
-  // drawVideo()
-  //exportPng()
 };
 
 // 非同期処理
 var drawImage = function drawImage(url, x, y, width, height) {
   return new Promise(function (resolve, reject) {
     var img = new Image();
-    img.src = frame;
+    img.src = url;
     img.onload = function () {
       ctx.drawImage(img, x, y, width, height);
       resolve('draw image');
@@ -71,10 +77,14 @@ var drawImage = function drawImage(url, x, y, width, height) {
   });
 };
 
-var drawText = function drawText(text) {
+var drawText = function drawText(text, x, y) {
   ctx.fillStyle = data.textColor;
-  ctx.font = "24px 'メイリオ'";
-  ctx.fillText(text, 10, 50);
+  ctx.font = "36px 'メイリオ'";
+  var textList = text.split('\n');
+  var lineHeight = ctx.measureText("あ").width;
+  textList.forEach(function (text, i) {
+    ctx.fillText(text, x, y + lineHeight * i);
+  });
 };
 
 var drawVideo = function drawVideo() {
@@ -84,7 +94,13 @@ var drawVideo = function drawVideo() {
 // 現在のcnavasのモノをbase64で返す
 var exportPng = function exportPng() {
   var data = canvas.toDataURL("image/png");
-  console.log(data);
+  return data;
+};
+
+var exportVideo = function exportVideo() {
+  var output = encoder.compile();
+  var url = webkitURL.createObjectURL(output);
+  console.log('url');
 };
 
 window.onload = function () {
