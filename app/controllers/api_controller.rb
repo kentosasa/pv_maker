56cb96032e6a5ca9c1964cfe9ae8c24e571d4af8
kento sasamoto <knote0721@gmail.com>
2017-03-03 13:22:39 +0900

    gifから動画生成

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,18 @@
 class ApiController < ApplicationController
+  require 'RMagick'
+  include Magick
+
   def upload
-    binding.pry
+    movie = Magick::ImageList.new
+    movie.ticks_per_second = 10
+    params['_json'].each do |item|
+      movie.push(Magick::Image.read_inline(item).first)
+      movie.cur_image.delay=10
+    end
+    movie.write("movie.gif")
+    system('ffmpeg -i movie.gif -pix_fmt yuv420p movie.mp4')
+    # ffmpeg -i movie.gif -pix_fmt yuv420p movie.mp4
+    # movie.write("movie")
     render :json => {hoge: 'fuga'}
   end
 end
